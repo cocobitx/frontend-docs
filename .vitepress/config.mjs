470cf19760e1a102101a2b4224755357f5ebdf51
Cristhian Dev. <cristhian.mahecha.dev@outlook.es>
2026-01-07 16:27:49 -0500

    feat: enlaces agregados en la navegación

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -7,10 +7,61 @@ export default defineConfig({
   title: "frontend",
   description: "guía práctica sobre tecnologías de frontend, estructura de carpetas, arquitectura y buenas prácticas de desarrollo",
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
+    logo: '/public/logo_v2.png',
+    search: {
+      provider: 'local',
+      options: {
+        placeholder: 'Buscar documentación'
+      }
+    },
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Inicio', link: '/' },
+      { text: 'Documentación', link: '/markdown-examples' },
+      {
+        text: 'Css3',
+        items: [
+          { text: 'Box shadow', link: 'https://box-shadow.dev/' },
+          { text: 'Box shadow gradients', link: 'https://alvarotrigo.com/shadow-gradients/' },
+          { text: 'clipath', link: 'https://bennettfeely.com/clippy/' },
+          { text: 'Buttons animations', link: 'https://www.cattocss.com/' },
+          { text: 'Transitions style', link: 'https://www.transition.style' },
+          { text: 'Loaders', link: 'https://cssloaders.github.io/' },
+          { text: 'Animatiss', link: 'https://xsgames.co/animatiss/' },
+          { text: 'AnimistaCss', link: 'https://animista.net/' },
+          { text: 'HoverCss', link: 'https://ianlunn.github.io/Hover/' },
+          { text: 'Components Css', link: 'https://uiverse.io/' },
+        ]
+      },
+      {
+        text: 'Javascript',
+        items: [
+          { text: 'AnimeJS', link: 'https://animejs.com/' },
+          { text: 'Motion', link: 'https://motion.dev/' }
+        ]
+      },
+      {
+        text: 'Iconos',
+        items: [
+          { text: 'tabler', link: 'https://tabler-icons.io/' },
+          { text: 'iconstack', link: 'https://iconstack.io/' },
+        ]
+      },
+      {
+        text: 'Practicas',
+        items: [
+          { text: 'devchallenge', link: 'https://devchallenges.io/' },
+          { text: 'frontendmentor', link: 'https://www.frontendmentor.io/' }
+        ]
+      },
+      {
+        text: 'Recursos',
+        items: [
+          { text: 'collect ui', link: 'https://collectui.com/designs' },
+          { text: 'devdocs', link: 'https://devdocs.io/' },
+          { text: 'roadmap', link: 'https://roadmap.sh/frontend' },
+          { text: 'code image', link: 'https://app.codeimage.dev' },
+        ]
+      }
     ],
 
     sidebar: [
@@ -24,7 +75,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/cocobitx' }
     ]
   }
 })
